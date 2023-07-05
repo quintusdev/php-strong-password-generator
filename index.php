dcf4b764c183b127488c5b1d6fa9199edc7054b7
Quintusdev <quintusdev92@gmail.com>
2023-07-05 14:39:20 +0200

    ES. BASE 1 COMPLETATO

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
-
-
+    function randomPassword() {
+        /* Prendo il valore della lunghezza inserito dall'utente */
+        $passLength = $_GET['password'];
+        /* Dichiaro i  */
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!$%&?';
+        /* Dichiaro $PASS come array */
+        $pass = array();
+        /* Imposto la lunghezza -1 */
+        $alphaLength = strlen($alphabet) - 1;
+        /* Ciclo FOR per generare casualmente ogni singolo carattere */
+        for ($i = 0; $i < $passLength; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        /* Converto l'array in una stringa */
+        return implode($pass); 
+    }
 ?>
 
 <!DOCTYPE html>
@@ -17,10 +32,37 @@
     </head>
 
     <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    
+        <div class="container bg-black p-2">
+            <div>
+                <h1 class="text-primary text-center m-3">Password Generator</h1>
+            </div>
+            <div class="row m-4">
+                <div class="col-12 border rounded-2 bg-white m-2 p-3">
+                    <div class="col-12">
+                        <form action="index.php" method="GET">
+                            <!-- Inserimento Dati Utente -->
+                            <div class="form-group m-3">
+                                <label for="psw">Lunghezza password: </label>
+                                <!-- Faccio inserire all'utente la lunghezza in numero della password da generare-->
+                                <input type="text" name="password" id="password" class="w-25 m-1" placeholder="Inserire il numero di caratteri">
+                            </div>
+                             <!-- Visualizzazione dati -->
+                            <div class="form-group m-3">
+                                <label for="pswgen" class="mt-2">Password Generata: </label>
+                                <!-- Invoco la funzione per far visualizzare la password generata -->
+                                <?php if($_GET['password'] > 0 && $_GET['password']< 32) { ?>
+                                    <h4><?php echo randomPassword(); ?></h4>
+                                <?php } else { ?>
+                                    <div class="alert alert-danger"><h6>"Attenzione! Lunghezza minima 1 e massima 32 caratteri."</h6></div>
+                                <?php } ?>
+                            </div>
+                            <!-- Bottoni per gestione dati -->
+                            <div class="form-group m-3">
+                                <button type="submit" class="btn btn-sm btn-success mt-2">Genera</button>
+                                <button type="reset" class="btn btn-sm btn-danger mt-2">Reset</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
